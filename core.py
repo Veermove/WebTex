fb609945c5bb20541747876c870156afa0a1724c
Tymoteusz Tretowicz <tymoteusz.tretowicz@nokia.com>
2022-06-14 09:13:43 +0200

    Generatrion working

--- core.py
+++ core.py
@@ -1,20 +1,20 @@
 import subprocess
+import os
+import time
 from werkzeug.datastructures import FileStorage
 from commons import Result, CompErrInfo
-import os
 
-def compile(file: FileStorage) -> Result:
+def compile_file(file: FileStorage) -> Result:
     file.save('./resources/t_file.tex')
-    
-    result_code = 0
-    err = None
-    try:
-        result_code = subprocess.check_call(                                                             \
-            ['pdflatex', '-output-directory' '/resources', '-file-line-error', 'resources/t_file.tex']   \
-        )
-        
-    except subprocess.CalledProcessError as e:
-        result_code = e.returncode
-        err = CompErrInfo(e.returncode, e.stderr)
+    # return compile_saved()sss
 
-    return Result(None, None)
+def save_and_compile(text) -> Result:
+    t_file = open('./resources/t_file.tex', 'w')
+    t_file.write(text)
+    t_file.close()
+    return compile_saved()
+
+def compile_saved() -> Result:
+    result_code = 0
+    os.system("pdflatex -interaction nonstopmode -output-directory /resources resources/t_file.tex > NUL 2>&1")
+    time.sleep(1.5)
