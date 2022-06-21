691c2ae33296548cf1c6cf25b7734c61ebc7f656
Tymoteusz Tretowicz <tymoteusz.tretowicz@nokia.com>
2022-06-21 15:54:03 +0200

    cors not working

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 from flask import Flask
 from views import views
+from flask_cors import CORS
 
 app = Flask(__name__)
 app.register_blueprint(views, url_prefix="/")
+CORS(app)
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
