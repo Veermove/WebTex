7d53c2fb37e52ea695ed7a7cde36123a95efa520
Tymoteusz Tretowicz <tymoteusz.tretowicz@nokia.com>
2022-06-21 15:34:53 +0200

    odd

--- commons.py
+++
@@ -1,7 +0,0 @@
-ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg'])
-
-def is_filename_ok(filename: str) -> bool:
-    try:
-        return filename and filename.split(".")[-1] in ALLOWED_EXTENSIONS
-    except:
-        return False
